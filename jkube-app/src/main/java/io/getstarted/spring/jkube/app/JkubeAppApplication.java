e9947d5e5d827b734595e6f994dd460eef67197e
chandana79895 <101712974+chandana79895@users.noreply.github.com>
2022-09-20 15:13:20 +0530

    Add files via upload

---
+++ jkube-app/src/main/java/io/getstarted/spring/jkube/app/JkubeAppApplication.java
@@ -0,0 +1,13 @@
+package io.getstarted.spring.jkube.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JkubeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JkubeAppApplication.class, args);
+	}
+
+}
---
+++ jkube-app/src/test/java/io/getstarted/spring/jkube/app/JkubeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.getstarted.spring.jkube.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JkubeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
